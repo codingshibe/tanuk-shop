148a303b57990a12ca3ce2e98bdd770d81d5e700
matchashiba <recr38@gmail.com>
2020-02-26 21:22:22 +0000

    Changed font family of Header h4 to Trade Winds Added google fonts link to index.html

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
     const cartParams = this.props.cart;
     return (
       <div className="navbar bg-dark topBar">
-        <h4><i className="fas fa-leaf text-success"></i> Tanuk Shop</h4>
+        <h4 className="shop-name"><i className="fas fa-leaf text-success"></i> Tanuk Shop</h4>
         <span className="shoppingCart float-right">{this.props.quantity} {this.props.item} <i onClick={() => this.props.setView('cart', cartParams)} className="fas fa-shopping-cart"/></span>
       </div>
     );
