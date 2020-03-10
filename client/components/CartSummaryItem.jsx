c039751494b15aadfa126ba085022dcd4e5c6fe5
matchashiba <recr38@gmail.com>
2020-03-10 19:47:46 +0000

    Changed quantity adjuster button styles to remove outline Added pointer to cursor for quantity adjuster buttons

--- client/components/CartSummaryItem.jsx
+++ client/components/CartSummaryItem.jsx
@@ -17,7 +17,12 @@ class CartSummaryItem extends React.Component {
           <h4>{this.props.productName}</h4>
           <p className="text-muted">$ {(this.props.price / 100).toFixed(2)}</p>
           <p>{this.props.description}</p>
-          <div>Quantity: <span>{this.props.quantity}</span> <button className="btn btn-sm btn-outline-success" onClick={() => this.props.addItem(this.props.product)}><i className="fas fa-plus" /></button> <button className="btn btn-sm btn-outline-success" onClick={() => this.handleSubtract()}><i className="fas fa-minus"/></button></div>
+          <div>Quantity: <span className="btn-group">
+            <button className="btn quantity-toggle" onClick={() => this.props.addItem(this.props.product)}><i className="fas fa-plus" /></button>
+            <button className="btn quantity-amount" disabled>{this.props.quantity}</button>
+            <button className="btn quantity-toggle" onClick={() => this.handleSubtract()}><i className="fas fa-minus"/></button>
+          </span>
+          </div>
         </div>
       </div>
     );
